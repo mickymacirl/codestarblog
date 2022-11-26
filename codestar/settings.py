21c0c1da6e8666261a3ecdf2e90fa3f5a6a85225
mickymacirl <mickymacster@gmail.com>
2022-11-26 23:43:03 +0000

    test changes

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # This is a security setting that prevents the site from being loaded in an iframe.
 X_FRAME_OPTIONS = 'SAMEORIGIN'
